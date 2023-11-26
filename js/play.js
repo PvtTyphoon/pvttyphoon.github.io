2a208ac501fd5ede78cd684aa17f5cd27d4fe9c0
Laura <hashtyphoon@protonmail.com>
2023-11-26 21:57:11 +0100

    New Tank

--- js/play.js
+++
@@ -1,5 +0,0 @@
-myAudio = document.getElementById('audio');
-myAudio.addEventListener('canplaythrough', function() {
-  this.currentTime = Math.floor((Math.random() * 2500) + 1);
-  this.play();
-});
